45a0e6ac33e5d4029d86a135cb752d39cd7e349c
kunekunos <62517389+kunekunos@users.noreply.github.com>
2026-07-14 07:07:06 +0200

    blog: sitemap +part5

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,8 +2,10 @@ import type { MetadataRoute } from "next";
 import { articles } from "@/lib/articles";
 import { articlesPart2 } from "@/lib/articles-part2";
 import { articlesPart3 } from "@/lib/articles-part3";
+import { articlesPart4 } from "@/lib/articles-part4";
+import { articlesPart5 } from "@/lib/articles-part5";
 
-const allArticles = [...articles, ...articlesPart2, ...articlesPart3];
+const allArticles = [...articles, ...articlesPart2, ...articlesPart3, ...articlesPart4, ...articlesPart5];
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = "https://espaiemocions.es";
